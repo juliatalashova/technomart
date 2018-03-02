634fd6b160b47da41973ced020a39423ffd7e009
Julia <julia.gancheva1990@gmail.com>
2018-03-02 11:10:01 +0200

    Sign up form added

--- js/main.js
+++ js/main.js
@@ -1,101 +1,436 @@
-window.onload = function() {
-  var writeUsBtn = document.getElementById('write-us-btn');
-  if (typeof(writeUsBtn) != 'undefined' && writeUsBtn != null) {
-    var writeUsModal = document.getElementById('write-us-modal');
-    writeUsBtn.onclick = function() {
-      writeUsModal.classList.remove('error');
-      writeUsModal.classList.add('active');
-    };
-    document.getElementById('write-us-close').onclick = function() {
-      writeUsModal.classList.remove('active');
-    };
-    writeUsModal.querySelector('.cancel').onclick = function () {
-      writeUsModal.classList.remove('active');
-    };
+// window.onload = function() {
+//   var writeUsBtn = document.getElementById('write-us-btn');
+//   if (typeof(writeUsBtn) != 'undefined' && writeUsBtn != null) {
+//     var writeUsModal = document.getElementById('write-us-modal');
+//     writeUsBtn.onclick = function() {
+//       writeUsModal.classList.remove('error');
+//       writeUsModal.classList.add('active');
+//     };
+//     document.getElementById('write-us-close').onclick = function() {
+//       writeUsModal.classList.remove('active');
+//     };
+//     writeUsModal.querySelector('.cancel').onclick = function () {
+//       writeUsModal.classList.remove('active');
+//     };
+//
+//     var form = writeUsModal.querySelector('form');
+//     var userName = writeUsModal.querySelector('[name=user-name]');
+//     var userEmail = writeUsModal.querySelector('[name=email]');
+//     form.addEventListener('submit', function(event) {
+//       if (!(userName.value) || !(userEmail.value)) {
+//         event.preventDefault();
+//         writeUsModal.classList.remove("error");
+//         writeUsModal.classList.add("error");
+//       }
+//     });
+//   }
+//
+//   var mapImg = document.getElementById('map-img');
+//   if (typeof(mapImg) != 'undefined' && mapImg != null) {
+//     var mapModal = document.getElementById('map-modal');
+//     mapImg.onclick = function() {
+//       mapModal.style.display = 'block';
+//     };
+//     document.getElementById('map-close').onclick = function() {
+//       mapModal.style.display = 'none';
+//     }
+//   }
+//
+//   var infoClose = document.getElementById('info-close');
+//   if (typeof(infoClose) != 'undefined' && infoClose != null) {
+//     infoClose.addEventListener('click', function(event) {
+//       event.preventDefault();
+//       closeInfoModal();
+//     });
+//     document.querySelector('#info-modal .cancel').addEventListener('click', function(event) {
+//       event.preventDefault();
+//       closeInfoModal();
+//     });
+//   }
+//
+//   var serviceTabs = document.querySelectorAll('.service-tabs li');
+//   if (typeof(serviceTabs) != 'undefined' && serviceTabs != null) {
+//     var serviceFeatureClasses = [
+//       '.services-features-delivery',
+//       '.services-features-guarantee',
+//       '.services-features-credit'
+//     ];
+//     var serviceFeatures = document.querySelectorAll('.services-features div');
+//     for (var i = 0; i < serviceTabs.length; i++) {
+//       (function(i) {
+//         serviceTabs[i].onclick = function() {
+//           removeClass(serviceTabs, 'active');
+//           this.classList.add('active');
+//
+//           removeClass(serviceFeatures, 'active');
+//           serviceFeatures[i].classList.add('active');
+//         };
+//       })(i);
+//     }
+//   }
+// };
+//
+// function closeInfoModal() {
+//   document.getElementById('info-modal').style.display = 'none';
+// }
+//
+// function removeClass(array, className) {
+//   var i = 0,
+//     len = array.length;
+//   for (; i < len; i++) {
+//
+//     if (array[i].classList) {
+//       array[i].classList.remove(className);
+//     } else {
+//       var names = array[i].className.split(' '),
+//         j = 0,
+//         sublen = names.length;
+//
+//       for (; j < sublen; j++) {
+//         if (names[j] === className) {
+//           names[j] = "";
+//         }
+//       }
+//       array[i].className = names.join('');
+//     }
+//   }
+//}
 
-    var form = writeUsModal.querySelector('form');
-    var userName = writeUsModal.querySelector('[name=user-name]');
-    var userEmail = writeUsModal.querySelector('[name=email]');
-    form.addEventListener('submit', function(event) {
-      if (!(userName.value) || !(userEmail.value)) {
+function addToBookmarks() {
+    var bookmark = document.querySelectorAll('.bookmark');
+    console.log(bookmark);
+    var bookmarkNumber = document.querySelector('.bookmark-number');
+    var bookmarkCounter = parseInt(bookmarkNumber.textContent);
+    for (var i = 0; i < bookmark.length; i++) {
+        bookmark[i].addEventListener('click', function() {
+            console.log('clicked');
+            bookmarkNumber.innerHTML = ++bookmarkCounter;
+            var styleElem = document.head.appendChild(document.createElement("style"));
+            styleElem.innerHTML = ".icon-bookmark:before {color: #ffd180}";
+        });
+    }
+}
+
+addToBookmarks();
+
+function addToCart() {
+    var buyGood = document.querySelectorAll('.buy');
+    var cartProdNumber = document.querySelector('.cart-prod-number');
+    var cartProdCount = parseInt(cartProdNumber.textContent);
+    for (var i = 0; i < buyGood.length; i++) {
+        buyGood[i].addEventListener('click', function () {
+            document.getElementById('info-modal').style.display = 'block';
+           cartProdNumber.innerHTML = ++cartProdCount;
+            var styleElem = document.head.appendChild(document.createElement("style"));
+            styleElem.innerHTML = ".icon-basket:before {color: #ffd180}";
+        });
+    }
+}
+addToCart();
+
+
+
+var writeUsModal = document.getElementById('write-us-modal');
+
+
+function showWriteUsPopUp(){
+    var writeUsBtn = document.getElementById('write-us-btn');
+    writeUsBtn.addEventListener('click', function () {
         event.preventDefault();
-        writeUsModal.classList.remove("error");
-        writeUsModal.classList.add("error");
-      }
-    });
-  }
-
-  var mapImg = document.getElementById('map-img');
-  if (typeof(mapImg) != 'undefined' && mapImg != null) {
-    var mapModal = document.getElementById('map-modal');
-    mapImg.onclick = function() {
-      mapModal.style.display = 'block';
-    };
-    document.getElementById('map-close').onclick = function() {
-      mapModal.style.display = 'none';
-    }
-  }
-
-  var infoClose = document.getElementById('info-close');
-  if (typeof(infoClose) != 'undefined' && infoClose != null) {
-    infoClose.addEventListener('click', function(event) {
-      event.preventDefault();
-      closeInfoModal();
-    });
-    document.querySelector('#info-modal .cancel').addEventListener('click', function(event) {
-      event.preventDefault();
-      closeInfoModal();
-    });
-  }
-
-  var serviceTabs = document.querySelectorAll('.service-tabs li');
-  if (typeof(serviceTabs) != 'undefined' && serviceTabs != null) {
-    var serviceFeatureClasses = [
-      '.services-features-delivery',
-      '.services-features-guarantee',
-      '.services-features-credit'
-    ];
-    var serviceFeatures = document.querySelectorAll('.services-features div');
-    for (var i = 0; i < serviceTabs.length; i++) {
-      (function(i) {
-        serviceTabs[i].onclick = function() {
-          removeClass(serviceTabs, 'active');
-          this.classList.add('active');
-
-          removeClass(serviceFeatures, 'active');
-          serviceFeatures[i].classList.add('active');
-        };
-      })(i);
-    }
-  }
-};
-
-function showInfoModal() {
-  document.getElementById('info-modal').style.display = 'block';
+        writeUsModal.style.display = 'block';
+    })
 }
 
-function closeInfoModal() {
-  document.getElementById('info-modal').style.display = 'none';
+showWriteUsPopUp();
+function hideWriteUsPopup() {
+    var btnClose = document.getElementById('write-us-close');
+    btnClose.addEventListener('click', function () {
+        event.preventDefault();
+        writeUsModal.style.display = 'none';
+    })
 }
+hideWriteUsPopup();
 
-function removeClass(array, className) {
-  var i = 0,
-    len = array.length;
-  for (; i < len; i++) {
+function hideWriteUsPopUpCancelBtn() {
+    var cancelBtn = document.querySelector('.cancel');
+    cancelBtn.addEventListener('click', function () {
+        event.preventDefault();
+        writeUsModal.style.display = 'none';
+    })
+}
+hideWriteUsPopUpCancelBtn();
 
-    if (array[i].classList) {
-      array[i].classList.remove(className);
+
+function openServiceFeatures(evt, serviceFeature) {
+    var i, serviceTabs, serviceFeatures;
+    serviceTabs = document.querySelectorAll('.service-tab');
+    for(i = 0; i < serviceTabs.length; i++) {
+        serviceTabs[i].className = serviceTabs[i].className.replace(' active', '');
+
+    }
+    serviceFeatures = document.querySelectorAll('.services-feature');
+    for(i = 0; i < serviceFeatures.length; i++) {
+        serviceFeatures[i].className = serviceFeatures[i].className.replace(' active', '');
+        serviceFeatures[i].style.display = 'none';
+    }
+    document.getElementById(serviceFeature).style.display = 'block';
+    evt.currentTarget.className += ' active';
+}
+document.getElementById('defaultOpen').click();
+
+
+function validateUserName() {
+    var username = document.forms['write-us-form']['user-name'];
+    var letters = /^[A-Za-z]+$/;
+    if(username.value.match(letters)) {
+        return true;
     } else {
-      var names = array[i].className.split(' '),
-        j = 0,
-        sublen = names.length;
-
-      for (; j < sublen; j++) {
-        if (names[j] === className) {
-          names[j] = "";
-        }
-      }
-      array[i].className = names.join('');
+        username.focus();
+        return false;
     }
-  }
 }
+validateUserName();
+var email = document.forms['write-us-form']['email'];
+var comment = document.forms['write-us-form']['comments'];
+function showSignUpForm() {
+    var signUpBtn = document.querySelector('.btn-reg');
+    console.log(signUpBtn);
+    signUpBtn.addEventListener('click', function () {
+        signUpForm.style.display = 'block';
+    })
+}
+showSignUpForm();
+function hideSignUpForm() {
+    var btnCancel = signUpForm.querySelector('.signUp-btn-cancel');
+    console.log(btnCancel);
+}
+hideSignUpForm();
+//var signUpForm = document.querySelector('.signUp-form');
+function showSignUpPopUp() {
+    var signUpBtn = document.querySelector('.btn-reg');
+
+    signUpBtn.addEventListener('click', function () {
+        event.preventDefault();
+        signUpForm.style.display = 'block';
+    })
+}
+showSignUpPopUp();
+function hideSignUpPopUp() {
+    var btnCancel = document.querySelector('.signUp-btn-cancel');
+    console.log(btnCancel);
+    btnCancel.addEventListener('click', function () {
+        event.preventDefault();
+        signUpForm.style.display = 'none';
+    })
+}
+hideSignUpPopUp();
+function hideSignUpFormWithCloseBtn() {
+    var btnClose = signUpForm.querySelector('.modal-content-close');
+    btnClose.addEventListener('click', function () {
+        event.preventDefault();
+        signUpForm.style.display = 'none';
+    })
+}
+hideSignUpFormWithCloseBtn();
+
+
+var signUpForm = document.querySelector('.signUp-form');
+var signUpFormFields = signUpForm.querySelectorAll('input');
+var pwdField = signUpForm.querySelector('#password').value;
+var confirmPwdField = signUpForm.querySelector('#confirm-password').value;
+var lowerCaseLetters = /[a-z]/g;
+var numbers = /[0-9]/g;
+
+for(var i = 0; i < signUpFormFields.length; i++) {
+    (function(i) {
+        signUpFormFields[i].onblur = function () {
+            /*var password = if (passwrod) sdfsds : false;
+            if (repassword && password == repassword) {
+                safasds
+            }*/
+            var pwd = function() {
+                if(pwdField === confirmPwdField && confirmPwdField === pwdField){
+                    return true;
+                } else {
+                    return false;
+                }
+            };
+            var isValidField = isValidForm(signUpFormFields[i].name, signUpFormFields[i].value);
+            if (isValidField || pwd) {
+                signUpFormFields[i].style.borderColor = 'green';
+            } else {
+                signUpFormFields[i].style.borderColor = 'red';
+            }
+        };
+    })(i)
+}
+/*var isValidPwd = function () {
+    console.log(pwdField.match(lowerCaseLetters));
+    if(pwdField !== confirmPwdField) {
+        return false;
+    } else if (pwdField == null && confirmPwdField == null) {
+        return false;
+    } else if (!pwdField.match(lowerCaseLetters) && !confirmPwdField.match(lowerCaseLetters)) {
+        return true;
+    } else if (!pwdField.match(numbers) && !confirmPwdField.match(numbers)) {
+        return true;
+    } else if (pwdField.length > 6 && confirmPwdField.length > 6) {
+        return false;
+    } else {
+        return true;
+    }
+
+};*/
+
+
+
+
+function removeError() {
+    var errors = signUpForm.querySelectorAll('.error');
+
+    for (var j = 0; j < errors.length; j++) {
+        errors[j].remove();
+    }
+}
+signUpForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+
+    for(var i = 0; i < signUpFormFields.length; i++) {
+
+        if (!signUpFormFields[i].value) {
+            console.log(signUpFormFields[i], 'field is blank');
+            var error = document.createElement('div');
+            error.className = 'error';
+            error.style.color = 'red';
+            error.innerHTML = 'Cannot be empty';
+            signUpForm[i].parentElement.insertBefore(error, signUpFormFields[i]);
+            signUpFormFields[i].style.borderColor = 'red';
+        } else {
+            signUpFormFields[i].style.borderColor = 'green';
+        }
+    }
+
+});
+
+function isValidForm(name, value) {
+    if (name === "email") {
+        if(value === " ") {
+            return false;
+        } else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    if(name === "user-name" || name == "last-name" || name == "middle-name") {
+        if(value == " ") {
+            return false;
+        } else if(value <= 2 ){
+            return false;
+        } else if(/^[0-9]+$/.test(value)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    if (name == "pwd" || name == "confirm-pwd") {
+        if(value == " ") {
+            return false;
+        } else if(!value.match(lowerCaseLetters)) {
+            return false;
+        } else if (!value.match(numbers)) {
+            return false;
+        } else if(value.length > 8) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+function signUpFormValidate(event) {
+    event.preventDefault(); // event обьекта нет, это хорошо, но тебе нужно написать подписку на событие без onsubmit
+    console.log('empty');
+    /*console.log('clicked on validate');
+    var errors = signUpForm.querySelectorAll('.error');
+
+    for (var j = 0; j < errors.length; j++) {
+        errors[j].remove();
+    }
+
+    for(var i = 0; i <= signUpFormFields.length; i++) {
+        if(!signUpFormFields[i].value) {
+            console.log(signUpFormFields[i], 'field is blank');
+            var error = document.createElement('div');
+            error.className='error';
+            error.style.color = 'red';
+            error.innerHTML = 'Cannot be empty';
+            signUpForm[i].parentElement.insertBefore(error, signUpFormFields[i]);
+            signUpFormFields[i].style.borderColor = 'red';
+        }
+    }*/
+
+}
+
+/*
+//function validateSignUpForm() {
+    var inputs = document.querySelectorAll('#signUpForm input[type=text]');
+    var pwdInputs = document.querySelectorAll('#signUpForm input[type=password]');
+    var signUpEmail = document.querySelector('#signUpForm input[type=email');
+    var lowerCaseLetters = /[a-z]/g;
+    var upperCaseLetters = /[A-Z]/g;
+    var numbers = /[0-9]/g;
+    var isValid = true;
+    console.log(signUpEmail, inputs);
+        for (var i = 0; i < inputs.length; i++) {
+            if (inputs[i].value == null || inputs[i].value == " ") {
+                inputs[i].style.borderColor = 'red';
+                isValid = false;
+            } else if (inputs[i].value <= 2) {
+                inputs[i].style.borderColor = 'red';
+                isValid = false;
+            } else if (/^[0-9]+$/.test(inputs[i].value)) {
+                inputs[i].style.borderColor = 'red';
+                isValid = false;
+            } else {
+                inputs[i].style.borderColor = 'green';
+                isValid = true;
+            }
+            inputs[i].onblur = function() {
+                if (isValid = false) {
+                    inputs[i].style.borderColor = 'red';
+                } else {
+                    inputs[i].style.borderColor = 'green';
+                }
+            }
+            console.log(inputs[i]);
+        }
+    signUpEmail.onblur = function () {
+        if(signUpEmail.value === " ") {
+            signUpEmail.style.borderColor = 'red';
+        } else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(signUpEmail.value)) {
+            signUpEmail.style.borderColor = 'green';
+        } else {
+            signUpEmail.style.borderColor = 'red';
+        }
+    };
+    for(var j = 0; j < pwdInputs.length; j++) {
+        if (pwdInputs[j].value == null || pwdInputs[j].value == " ") {
+            pwdInputs[j].style.borderColor = 'red';
+    } else if(pwdInputs[0] !== pwdInputs[1]){
+            pwdInputs[j].style.borderColor = 'red';
+        } else if(!pwdInputs[j].value.match(lowerCaseLetters)){
+            pwdInputs[j].style.borderColor = 'red';
+        } else if(!pwdInputs[j].value.match(upperCaseLetters)) {
+            pwdInputs[j].style.borderColor = 'red';
+        } else if(!pwdInputs[j].value.match(numbers)) {
+            pwdInputs[j].style.borderColor = 'red';
+        } else if(!pwdInputs[j].value.length >= 8) {
+            pwdInputs[j].style.borderColor = 'red';
+        } else {
+            pwdInputs[j].style.borderColor = 'green';
+        }
+    }
+//}
+//validateSignUpForm();*/
---
+++ js/new.js
@@ -0,0 +1,228 @@
+if(document.contains(document.getElementById('signUpForm'))) {
+    var signUpForm = document.getElementById('signUpForm');
+    var signUpCloseBtn = signUpForm.querySelector('.modal-content-close');
+
+    function showSignUpForm() {
+        signUpForm.style.display = 'block';
+    }
+
+    document.querySelector('.btn-reg').addEventListener('click', showSignUpForm);
+
+
+    function validateTextFields() {
+        var isValid = true;
+        var signUpTextFields = signUpForm.querySelectorAll('input[type="text"]');
+        for (var i = 0; i < signUpTextFields.length; i++) {
+
+            if (!signUpTextFields[i].value) {
+                signUpTextFields[i].classList.add('field-invalid');
+                isValid = false;
+            } else if (/^[0-9]+$/.test(signUpTextFields[i].value)) {
+                signUpTextFields[i].classList.add('field-invalid');
+            } else if (signUpTextFields[i].value.length <= 2) {
+                signUpTextFields[i].classList.add('field-invalid');
+                isValid = false;
+            } else {
+                signUpTextFields[i].classList.remove('field-invalid');
+                signUpTextFields[i].classList.add('field-valid');
+            }
+        }
+        return isValid;
+    }
+
+    function validateEmail() {
+        var isValid = true;
+        var fieldEmail = signUpForm.querySelector('input[type="email"]');
+        if (fieldEmail.value == '') {
+            fieldEmail.classList.add('field-invalid');
+            isValid = false;
+        } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(fieldEmail.value)) {
+            fieldEmail.classList.add('field-invalid');
+            isValid = false;
+        } else {
+            fieldEmail.classList.remove('field-invalid');
+            fieldEmail.classList.add('field-valid');
+        }
+        return isValid;
+    }
+
+    function validatePwd() {
+        var isValid = true;
+        var fieldsPwd = signUpForm.querySelectorAll('input[type="password"]');
+        var lowerCaseLetters = /[a-z]/g;
+        var upperCaseLetters = /[A-Z]/g;
+        var numbers = /[0-9]/g;
+        for (var i = 0; i < fieldsPwd.length; i++) {
+            if (!fieldsPwd[i].value) {
+                fieldsPwd[i].classList.add('field-invalid');
+                isValid = false;
+            } else if (!fieldsPwd[i].value.match(lowerCaseLetters)) {
+                fieldsPwd[i].classList.add('field-invalid');
+                isValid = false;
+            } else if (!fieldsPwd[i].value.match(numbers)) {
+                fieldsPwd[i].classList.add('field-invalid');
+                isValid = false;
+            } else if (!fieldsPwd[i].value.match(upperCaseLetters)) {
+                fieldsPwd[i].classList.add('field-invalid');
+                isValid = false;
+            } else if (fieldsPwd[i].value.length >= 8) {
+                fieldsPwd[i].classList.add('field-valid');
+
+            } else if (fieldsPwd[i][0] === fieldsPwd[i][1]) {
+                fieldsPwd[i].classList.add('field-valid');
+
+            } else {
+                fieldsPwd[i].classList.remove('field-invalid');
+                fieldsPwd[i].classList.add('field-valid');
+            }
+        }
+        return isValid;
+    }
+
+    function removeClass() {
+        var signUpFormFields = signUpForm.querySelectorAll('input, textarea');
+        for (var i = 0; i < signUpFormFields.length; i++) {
+            if (signUpFormFields[i].classList.contains('field-invalid')) {
+                signUpFormFields[i].classList.remove('field-invalid')
+            } else if (signUpFormFields[i].classList.contains('field-valid')) {
+                signUpFormFields[i].classList.remove('field-valid');
+            }
+        }
+    }
+    function hideSignUpForm() {
+        signUpForm.reset();
+        removeClass();
+        signUpForm.style.display = 'none';
+    }
+    signUpForm.querySelector('.signUp-btn-cancel').addEventListener('click', hideSignUpForm);
+
+    signUpCloseBtn.addEventListener('click', hideSignUpForm);
+
+
+    /*var formData = new FormData();
+    formData.append('firstname', 'Test');
+    formData.append('lastname', 'sdfsdf');
+    formData.append('password',  'dsfsdf');
+    formData.append('email', 'test@test.com');
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+    xhttp.open('POST', 'http://miffka1986.000webhostapp.com/register.php', true);
+    xhttp.send();*/
+
+    signUpForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        var isValidText = validateTextFields();
+        var isValidEmail = validateEmail();
+        var isValidPwd = validatePwd();
+        if (isValidText == true && isValidEmail == true && isValidPwd == true) {
+            //signUpForm.submit();
+            var formData = new FormData(signUpForm);
+
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log(this.responseText);
+                }
+            };
+            xhttp.open('POST', 'https://miffka1986.000webhostapp.com/register.php', true);
+            xhttp.send(formData);
+        }
+    });
+}
+if (document.contains(document.getElementById('write-us-modal'))) {
+    var writeUsModal = document.getElementById('write-us-modal');
+    var writeUsCancelBtn = writeUsModal.querySelector('.cancel');
+
+    function showWriteUsPopUp() {
+        writeUsModal.style.display = 'block';
+    }
+
+    function hideWriteUsPopup() {
+        writeUsModal.style.display = 'none';
+    }
+
+    document.getElementById('write-us-btn').addEventListener('click', showWriteUsPopUp);
+
+    writeUsModal.querySelector('#write-us-close').addEventListener('click', hideWriteUsPopup);
+
+    writeUsCancelBtn.addEventListener('click', hideWriteUsPopup);
+
+}
+if (document.contains(document.querySelector('.service-tabs'))) {
+    function openServiceFeatures(evt, serviceFeature) {
+        var i, serviceTabs, serviceFeatures;
+        serviceTabs = document.querySelectorAll('.service-tab');
+        for (i = 0; i < serviceTabs.length; i++) {
+            serviceTabs[i].className = serviceTabs[i].className.replace(' active', '');
+
+        }
+        serviceFeatures = document.querySelectorAll('.services-feature');
+        for (i = 0; i < serviceFeatures.length; i++) {
+            serviceFeatures[i].className = serviceFeatures[i].className.replace(' active', '');
+            serviceFeatures[i].style.display = 'none';
+        }
+        document.getElementById(serviceFeature).style.display = 'block';
+        evt.currentTarget.className += ' active';
+    }
+
+    document.getElementById('defaultOpen').click();
+}
+
+var addedToCartInfo = document.querySelector('.modal-content-info');
+var infoModalCloseBtn = addedToCartInfo.querySelector('.modal-content-close');
+var continueBuyingBtn = addedToCartInfo.querySelector('.cancel');
+
+function hideAddedToCartPopUp() {
+    addedToCartInfo.style.display = 'none';
+}
+infoModalCloseBtn.addEventListener('click', hideAddedToCartPopUp);
+
+continueBuyingBtn.addEventListener('click', hideAddedToCartPopUp);
+
+
+
+//signUpForm.addEventListener('blur', hideSignUpForm);
+
+function addToCart() {
+    var buyGood = document.querySelectorAll('.buy');
+    var cartProdNumber = document.querySelector('.cart-prod-number');
+    var cartProdCount = parseInt(cartProdNumber.textContent);
+    for (var i = 0; i < buyGood.length; i++) {
+        buyGood[i].addEventListener('click', function () {
+            document.getElementById('info-modal').style.display = 'block';
+            cartProdNumber.innerHTML = ++cartProdCount;
+            var styleElem = document.head.appendChild(document.createElement("style"));
+            styleElem.innerHTML = ".icon-basket:before {color: #ffd180}";
+        });
+    }
+}
+addToCart();
+
+function addToBookmarks() {
+    var bookmark = document.querySelectorAll('.bookmark');
+    var bookmarkNumber = document.querySelector('.bookmark-number');
+    var bookmarkCounter = parseInt(bookmarkNumber.textContent);
+    for (var i = 0; i < bookmark.length; i++) {
+        bookmark[i].addEventListener('click', function() {
+            bookmarkNumber.innerHTML = ++bookmarkCounter;
+            var styleElem = document.head.appendChild(document.createElement("style"));
+            styleElem.innerHTML = ".icon-bookmark:before {color: #ffd180}";
+        });
+    }
+}
+
+addToBookmarks();
+
+
+
+
+
+
+
+
+
+
